e91faf958210658a7c6f9b62d799e7324cd81353
Alex Jimmy <alu0100728170@ull.edu.es>
2017-02-17 16:20:47 +0000

    gulpfile.js creado. Tareas implementadas

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict'
+var
+    gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    pug = require('gulp-pug'),
+    sass = require('gulp-sass');
+
+    gulp.task('sass', () => {
+        return gulp.src(`sass/**/*.scss`)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(`app/css`));
+    })
+    gulp.task('browserSync', () => {
+        browserSync.init({
+            server: { baseDir: 'app'},
+            browser: "firefox" // Indica que navegador se utilizará
+        })
+    });
+    gulp.task('pug', ()=> {
+        return gulp.src([`pug/**/*.pug`,`!pug/_partials/**/*.pug` ])
+            .pipe(pug({
+                pretty: true
+            }))
+            .pipe(gulp.dest(`app`));
+    })
+
+    gulp.task('simple-web',['browserSync'], () => {
+        console.log("Iniciando...");
+        gulp.watch(`sass/**/*.scss`, ['sass', browserSync.reload]);
+        gulp.watch(`pug/**/*.pug`, ['pug', browserSync.reload]);
+        // gulp.watch(folder + '/app/**/*.{html,css,js}', browserSync.reload);
+
+    });
